6dd5aa2150c6f0a93c29715fdeb1e458c8aadb2c
shasingh <sharandeep.singh@niceactimize.com>
2020-06-20 15:10:16 +0530

    added variable to welcome page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [];
+    $data["limerick"] = "Just kidding";
+    return view('welcome',$data);
 });
