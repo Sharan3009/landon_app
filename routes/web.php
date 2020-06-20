64be56569fcddb246a312fd5a77600888e8475ff
shasingh <sharandeep.singh@niceactimize.com>
2020-06-20 22:58:45 +0530

    named routing

--- routes/web.php
+++ routes/web.php
@@ -12,23 +12,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', "ContentsController@home");
+Route::get('/', "ContentsController@home")->name("home");
 
-Route::get('/clients', "ClientController@index");
+Route::get('/clients', "ClientController@index")->name('clients');
 
-Route::get('clients/new', "ClientController@newClient");
+Route::get('clients/new', "ClientController@newClient")->name('new_client');
 
-Route::post('clients/new', "ClientController@create");
+Route::post('clients/new', "ClientController@create")->name('create_client');
 
-Route::get('clients/{client_id}', "ClientController@show");
+Route::get('clients/{client_id}', "ClientController@show")->name('show_client');
 
-Route::post('clients/{client_id}', "ClientController@modify");
+Route::post('clients/{client_id}', "ClientController@modify")->name('update_client');
 
-Route::get('reservations/{client_id}', "RoomsController@checkAvailableRooms");
+Route::get('reservations/{client_id}', "RoomsController@checkAvailableRooms")->name('check_room');
 
-Route::post('reservations/{client_id}', "RoomsController@checkAvailableRooms");
+Route::post('reservations/{client_id}', "RoomsController@checkAvailableRooms")->name('check_room');
 
-Route::get('book/room/{client_id}/{room_id}/{date_in}/{date_out}', "ReservationsController@bookRoom");
+Route::get('book/room/{client_id}/{room_id}/{date_in}/{date_out}', "ReservationsController@bookRoom"->name('book_room'));
 
 Route::get('/home', function () {
     $data = [];
