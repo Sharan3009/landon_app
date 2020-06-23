8eeca9b6a8d63861607f7215fa6b6a3989205790
shasingh <sharandeep.singh@niceactimize.com>
2020-06-23 14:34:58 +0530

    showing rooms to view

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Client;
+use App\Room;
 
 class RoomsController extends Controller
 {
@@ -17,6 +19,6 @@ class RoomsController extends Controller
         $data['dateTo'] = $dateTo;
         $data['rooms'] = $room->getAvailableRooms($dateFrom,$dateTo);
         $data['client'] = $client->find($client_id);
-        return view("rooms/checkAvailableRooms");
+        return view("rooms/checkAvailableRooms",$data);
     }
 }
